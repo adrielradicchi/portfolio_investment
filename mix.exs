9898a0c2d9a19ce6bb141caff88b2a8559955ded
Adriel Radicchi <adriel.nfsc@gmail.com>
2020-11-26 18:50:14 -0300

    remove lib cors_plug the production

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule PortfolioInvestment.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:argon2_elixir, "~> 2.0"},
       {:tesla, "~> 1.3.0"},
-      {:cors_plug, "~> 2.0"}
+      {:cors_plug, "~> 2.0", only: :dev}
     ]
   end
 
