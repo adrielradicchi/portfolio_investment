eb75c42aa3e9c45746616d280fb06f7d25ce7e7c
Adriel Radicchi <adriel.nfsc@gmail.com>
2020-11-27 09:12:19 -0300

    deploy

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -5,31 +5,17 @@
 use Mix.Config
 
 
-database_url =
-  Map.fetch!(System.get_env(), "DATABASE_URL") || ""
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
-
 config :portfolio_investment, PortfolioInvestment.Repo,
-  url: database_url,
+  url: Map.fetch!(System.get_env(), "DATABASE_URL"),
   ssl: true,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
-
-secret_key_base =
-  Map.fetch!(System.get_env(), "SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
 
 config :portfolio_investment, PortfolioInvestmentWeb.Endpoint,
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # ## Using releases (Elixir v1.9+)
 #
