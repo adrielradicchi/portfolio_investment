cb003e1ff80d29e1194c42e65559ff722015f843
Adriel Radicchi <adriel.nfsc@gmail.com>
2020-11-30 14:42:01 -0300

    deploy

--- lib/portfolio_investment_web/controllers/users_controller.ex
+++ lib/portfolio_investment_web/controllers/users_controller.ex
@@ -39,12 +39,6 @@ defmodule PortfolioInvestmentWeb.UsersController do
 		|> handle_response(conn, "update.json", :ok)
 	end
 
-	def options(conn, _arams) do
-		conn
-		|> put_status(:ok)
-		|> text("")
-	end
-
 	defp handle_delete({:ok, _user}, conn) do
 		conn
 		|> put_status(:no_content)
